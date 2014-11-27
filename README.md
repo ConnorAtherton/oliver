538f3d09e84e16662dbbc2a3e4278050fe1c9ee6
Connor Atherton <connor@bitnami.com>
2014-11-27 11:45:13 -0800

    Add example and finish support

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-oliver
+Oliver
 ======
 
 Ask for permissions in the browser.
+
+> Please sir, can I have more desktop permissions.
+>
+> - Oliver Twist
+
+A small library to make issuing desktop notifications a breeze. [Demo](http://htmlpreview.github.io/?https://github.com/ConnorAtherton/oliver/blob/master/example/index.html)
+
+```js
+/* Create a new Oliver object. This acts
+ * as a factory for all notifications so set global
+ * options for all notifications here. They can be overwritten later
+ */
+var oliver = new Oliver({
+	title: 'It\'s Austin'
+});
+
+/* Create a notification overwriting any defaults.
+ * *title* and *body* are mandatory. Everything else is optional.
+ *
+ * See here for all options https://developer.mozilla.org/en-US/docs/Web/API/notification
+ */
+var notification = oliver.notify({
+	// title: 'Each notification is separate' -> would overwrite
+	body: 'Notify me baby',
+	icon: './austin.jpg',
+});
+
+// Attach events directly to the notification object
+notification.onclick = function() {
+	console.log('clicked the notificaton')
+};
+
+// Or add them as you normally would to any DOM element
+notification.addEventListener('click', function() {
+	console.log('Also clicked it.');
+}, false);
+
+```
--- src/oliver.js
+++ src/oliver.js
@@ -22,7 +22,70 @@
 }(this, function factory(exports) {
   'use strict';
 
-  var Oliver = {};
+  var supportsNotifications = !!window.Notification && !!Notification.permission;
+
+  var defaults = {
+    title: "Notification",
+    lang: "en-us",
+    id: "browserNotification"
+  };
+
+  // Only available in moz, chrome and opera
+  function Oliver(opts) {
+    if (!(this instanceof Oliver))
+      return new Oliver(opts);
+
+    if (!supportsNotifications)
+      return this.error('noSupport');
+
+    this.opts = opts;
+    mixin(this.opts, defaults, true);
+  }
+
+  Oliver.prototype.error = function(key) {
+    var errors = {
+      'noSupport': 'This browser doesn\'t support desktop notifications.',
+      'noBody': 'A notification must have a body supplied.'
+    };
+
+    var msg = errors[key] || 'Oliver - Unknown error';
+    console.log('Oliver -', msg);
+  };
+
+  Oliver.prototype.notify = function(opts) {
+    mixin(this.opts, opts);
+
+    if (!this.opts.body) return this.error('noBody');
+
+    return this.askForPermission(function() {
+      return new Notification(this.opts.title, this.opts);
+    });
+  };
+
+  Oliver.prototype.askForPermission = function(cb) {
+    var self = this;
+    // already been granted permission
+    if (Notification.permission === "granted") return cb.call(this);
+
+    // if we have not already asked for permission
+    // and been denied
+    if (Notification.permission !== "denied") {
+      return Notification.requestPermission(function(permission) {
+        return permission === "granted" || permission === "default" ? cb.call(self) : false;
+      });
+    }
+  };
+
+  function mixin(target, source, skipIfPresent) {
+    for (var prop in source) {
+      if (source.hasOwnProperty(prop)) {
+        if (target[prop] && skipIfPresent) continue;
+        target[prop] = source[prop];
+      }
+    }
+
+    return target;
+  }
 
   return Oliver;
 }));
